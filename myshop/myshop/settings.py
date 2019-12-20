0d39fbd6afaa1a03ba042a4974922c53de106b3c
HodorTaras <dvalindvorf@gmail.com>
2019-12-21 00:53:38 +0200

    Deploy Heroku try

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xc^c6&f&@c*8$(ip(w!eijo#%ka&u5j!&204==j_phnj@-t5i0'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -133,7 +134,7 @@ CART_SESSION_ID = 'cart'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-site-childstore@gmail.com
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
@@ -141,8 +142,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_dev'),
 )
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_envsite-childstore@gmail.com)
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'site-childstore@gmail.com'
